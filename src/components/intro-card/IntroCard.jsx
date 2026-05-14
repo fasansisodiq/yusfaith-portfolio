5fc62fc8f4843d0f1a9deee82b280e35a83c4d4d
Adewale Mudasiru <mudasiruluqmaan+coding@gmail.com>
2026-05-14 11:33:44 +0100

    ebute fix

--- src/components/intro-card/IntroCard.jsx
+++ src/components/intro-card/IntroCard.jsx
@@ -14,7 +14,7 @@ const IntroCard = ({ title, description, image, imageAlt }) => {
       <div className="w-full lg:h-screen flex flex-col text-center items-center justify-center gap-12 mt-12 lg:-mt-24">
         <div className="flex flex-col items-center gap-2 md:gap-0">
           <span className="w-fit flex items-center bg-purple-50/10 border border-blue-500/20 text-xs tracking-wide uppercase text-blue-500/70 p-2 rounded-full">
-            <IconPointFilled size={12} className="mr-1" />
+            <IconPointFilled size={14} className="mr-1 animate-pulse" />
             Terminal Management System
           </span>
           <h2 className="font-black uppercase text-[clamp(72px,12vw,160px)] leading-0.9">
@@ -36,12 +36,12 @@ const IntroCard = ({ title, description, image, imageAlt }) => {
           </div>
         </div>
       </div>
-      <div className="w-full md:full md:max-w-250 md:mx-auto border border-white/20 shadow-sm shadow-white/20 md:-mt-40 rounded-xl">
+      <div className="w-full md:full md:max-w-250 md:mx-auto bg-linear-to-br from-blue-500 via-transparent via-50% to-[#EB5017] p-0.5 shadow-sm shadow-white/20 mt-12 lg:-mt-40 rounded-xl">
         <img
           loading="lazy"
           src={heroImg}
           alt={imageAlt}
-          className="w-full h-full"
+          className="w-full h-full rounded-lg"
         />
       </div>
     </section>
@@ -52,7 +52,7 @@ export default IntroCard;
 
 const InfoCard = ({ label, value }) => {
   return (
-    <div className="flex flex-col gap-2 p-6">
+    <div className="flex flex-col gap-2 p-3 lg:p-6">
       <span
         className="text-xs tracking-widest text-muted-foreground
       "
