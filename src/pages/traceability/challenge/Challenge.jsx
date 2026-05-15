4cc3c92ab54ad3a38d81257dae44a163adb86802
Adewale Mudasiru <mudasiruluqmaan+coding@gmail.com>
2026-05-15 23:13:43 +0100

    grid adjust

--- src/pages/traceability/challenge/Challenge.jsx
+++ src/pages/traceability/challenge/Challenge.jsx
@@ -16,7 +16,7 @@ const Challenge = () => {
         />
       </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
         {challenges.map((item, idx) => {
           return (
             <div
